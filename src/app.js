f5aaf8a74535e0fc5a20baa9faa43dfa53ed7d59
pranshu-rel <pranshu.soni@ril.com>
2026-01-14 12:16:33 +0530

    log requested

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ const express = require("express");
 const { createLogger } = require("@your-scope/kafka-logger");
 
 const app = express();
-
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json())
 
 const logger = createLogger({
-  service: "test-service",
-  brokers: ["localhost:9092"], // ✅ FIX
+  service: "order-service",
+  brokers: ["localhost:9092"],
   topic: "application-logs",
 });
 
 app.post("/logs", async (req, res) => {
   try {
-    await logger.info("Hello from npm logger", req.body);
+    // await logger.info("Hello from npm logger", req.body);
+     logger.logRequest(req, { message: "Create order request" });
+     logger.
+
     res.status(200).json({ success: true });
   } catch (error) {
     console.error("Logger error:", error);
